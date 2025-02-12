272c1864da4f1e3707a28bc3dddddae8baf3a1f1
pranaysj <pranaysj3@gmail.com>
2025-02-12 16:25:27 +0530

    Render Ball
    Render Ball Sprite

--- Header/Gameplay/Ball/Ball.h
+++ Header/Gameplay/Ball/Ball.h
@@ -1,16 +1,26 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <string>
 
 namespace GamePlay {
 	class Ball {
 	private:
-		sf::CircleShape ballSprite;
-		const float radius = 10.0f;
+
+		sf::Texture ballTexture;
+		sf::Sprite ballSprite;
+
+		const std::string ballTexturePath = "Assets/Textures/Ball.png";
+
+		const float scaleX = 0.03f;
+		const float scaleY = 0.03f;
+
 		const float positonX = 615.0f;
 		const float positionY = 335.0f;
 
 	public:
 		Ball();
+		void LoadTexture();
+		void InitializeVariable();
 		void Update();
 		void Render(sf::RenderWindow* gameWindow);
 	};
--- Source/Gameplay/Ball/Ball.cpp
+++ Source/Gameplay/Ball/Ball.cpp
@@ -4,7 +4,21 @@ namespace GamePlay
 {
 	Ball::Ball()
 	{
-		ballSprite.setRadius(radius);
+		LoadTexture();
+		InitializeVariable();
+	}
+
+	void Ball::LoadTexture()
+	{
+		if (!ballTexture.loadFromFile(ballTexturePath)) {
+			throw std::runtime_error("Failed to load ball texture!");
+		}
+	}
+
+	void Ball::InitializeVariable()
+	{
+		ballSprite.setTexture(ballTexture);
+		ballSprite.setScale(scaleX, scaleY);
 		ballSprite.setPosition(positonX, positionY);
 	}
 
